ab5c4acf2fba71e4cd6e6ee72a202d814c72ab0a
Mohamud Mohamud <m.mohamud0001@gmail.com>
2026-07-16 22:15:15 +0300

    finished implementing test for bitset

--- include/bitset.h
+++ include/bitset.h
@@ -5,20 +5,27 @@
 
 typedef struct bitset BitSet;
 
-extern BitSet* bitset_init_bset(int byteSize);
+extern BitSet* bitset_init_bset(uint8_t bytesSize);     /* Initialize internal bitset with size bytesSize, range {1, 255} 0 default to 1 */
 
-extern void bitset_free_bset(BitSet* bset);
+extern void bitset_free_bset(BitSet* bset);             /* Free underlining memory */
 
-extern int8_t bitset_get_count(BitSet* bset);
+extern int16_t bitset_get_capacity_bits(BitSet* bset);  /* Returns the size of internal bit */
 
-extern int8_t bitset_get_size(BitSet* bset);
+extern int8_t bitset_get_capacity_bytes(BitSet* bset); /* Returns the size of internal bytes */
 
-extern void bitset_set_bit(uint32_t pos);
+extern int16_t bitset_get_size_bits(BitSet* bset);      /* Returns the amount of set bit */
 
-extern void bitset_set_all_bit(void);
+extern void bitset_set_bit_pos(BitSet* bSet, uint32_t pos); /* Sets a bit at a certain position, quietly exit on error */
 
-extern void bitset_reset_bit(int pos);
+extern void bitset_set_all_bit(BitSet* bSet);   /* Sets all the bits in the bitset */
 
-extern void bitset_reset_all_bit(int pos);
+extern void bitset_reset_bit_pos(BitSet* bSet, uint32_t pos);   /* Sets a bit at pos to zero */
+
+extern void bitset_reset_all_bit(BitSet* bSet);     /* Set all bits to zero */
+
+extern int8_t bitset_get_bit_pos(BitSet* bSet, uint32_t pos);   /* Return whether a bit is set or 0, return -1 on error */
+
+extern uint8_t* bitset_to_int_array(BitSet* bSet, uint8_t* retSize);    /* Return a array of integers that user need to free */
+                                                                        /* retSize is value is set to the returning array size */
 
 #endif
--- src/bitset.c
+++ src/bitset.c
@@ -1,13 +1,114 @@
 #include "bitset.h"
+#include "svec.h"
+
+#include <stdint.h>
+#include <assert.h>
+#include <string.h>
 
 
 struct bitset {
     uint8_t* set;
-    int capacity;
-    int set_bit_num;
+    uint16_t capacity;
 };
 
-BitSet* bitset_init_bset(int byteSize) {
+BitSet* bitset_init_bset(uint8_t bytesSize) {
+    BitSet* bSet = xmalloc(sizeof(*bSet));
+   
+    if (bytesSize == 0) {
+        bytesSize = 1;
+    }
+    bSet->set = xmalloc(sizeof(*bSet->set) * bytesSize);
+    bSet->capacity = ((sizeof(*bSet->set) * bytesSize) * 8 ) - 1;
+
+    return bSet;
+}
+
+void bitset_free_bset(BitSet* bSet) {
+    if (bSet == NULL) {
+        return;
+    }
+    free(bSet->set);
+    free(bSet);
+}
+
+int16_t bitset_get_capacity_bits(BitSet* bSet) {
+    assert(bSet != NULL);
+    return bSet->capacity;
+}
+
+int8_t bitset_get_capacity_bytes(BitSet* bSet) {
+    assert(bSet != NULL);
+    return (bSet->capacity + 1) / 8;
+}
+
+int16_t bitset_get_size_bits(BitSet* bSet) {
+    assert(bSet != NULL);
+    int16_t resulted = 0;
+
+    for (uint32_t i = 0; i < bSet->capacity + 1; i++) {
+        resulted += (*bSet->set >> i) & 1;
+    }
+    return resulted;
+}
+
+void bitset_set_bit(BitSet* bSet, uint32_t pos) {
+    assert(bSet != NULL);
+    if (pos > bSet->capacity) {
+        return; 
+    }
+    int idx = pos / 8;
+    int x = pos % 8;
+
+    bSet->set[idx] |= (1 << x);
+}
+
+void bitset_set_all_bit(BitSet* bSet) {
+    assert(bSet != NULL);
+    uint32_t byteSize = bitset_get_capacity_bytes(bSet);
+    for (uint32_t i = 0; i < byteSize; i++) {
+        bSet->set[i] = 255;
+    }
+}
+
+void bitset_reset_bit(BitSet* bSet, uint32_t pos) {
+    assert(bSet != NULL);
+    if (pos > bSet->capacity) {
+        return; 
+    }
+
+    int idx = pos / 8;
+    int x = pos % 8;
+    bSet->set[idx] &= (0 << x);
+}
+
+void bitset_reset_all_bit(BitSet* bSet) {
+    assert(bSet != NULL);
+    uint32_t byteSize = bitset_get_capacity_bytes(bSet);
+    for (uint32_t i = 0; i < byteSize; i++) {
+        bSet->set[i] = 0;
+    }
+}
+
+
+int8_t bitset_get_bit(BitSet* bSet, uint32_t pos) {
+    assert(bSet != NULL);
+    if (pos > bSet->capacity) {
+        return 0; 
+    }
+    int idx = pos / 8;
+    int x = pos % 8;
+
+    return (bSet->set[idx] >> x) & 1;
+}
+
+uint8_t* bitset_to_int_array(BitSet* bSet, uint8_t* retSize) {
+    assert(bSet != NULL);
+    uint8_t bytesSize = (bSet->capacity + 1) / 8;
+    uint8_t* retArr = xmalloc(bytesSize);
+
+    memcpy(retArr, bSet->set, bytesSize);
+    *retSize = bytesSize;
+    return retArr;
     
 }
 
